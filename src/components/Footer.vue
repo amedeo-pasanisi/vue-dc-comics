af64941ead94ddc3107782a2537a33a39e7ee3be
amedeo <amedeopasanisi97@gmail.com>
2021-09-20 18:02:06 +0200

    fine scaffolding

---
+++ src/components/Footer.vue
@@ -0,0 +1,44 @@
+<template>
+  <footer>
+    <div class="top">
+        <div class="container">
+            <h1>footer top</h1>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="container">
+            <h1>footer bottom</h1>
+        </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
+
+<style scoped lang="scss">
+@import "../style/variables";
+@import "../style/mixins";
+
+footer {
+    .top {
+        padding: 20px 0;
+        background-image: url("../assets/img/footer-bg.jpg");
+        color: white;
+    }
+    .bottom {
+        padding: 20px 0;
+        background-color: $text-black;
+        color: white;
+    }
+    .container {
+    @include container;
+    display: flex;
+    align-items: center;
+    height: 100px;
+    }
+}
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <div class="top">
+        <div class="container">
+            <h1>Main top</h1>
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="container">
+            <h1>Main bottom</h1>
+        </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+<style scoped lang="scss">
+@import "../style/variables";
+@import "../style/mixins";
+
+main {
+    .top {
+        padding: 20px 0;
+        background-color: black;
+        color: white;
+    }
+    .bottom {
+        padding: 20px 0;
+        background-color: $blue;
+        color: white;
+    }
+    .container {
+    @include container;
+    display: flex;
+    align-items: center;
+    height: 100px;
+    }
+}
+</style>
